4a26155f188f29390269f64d74fdae043987d4e2
Edwin DeJesus <edwinstoddart.student@gmail.com>
2021-09-08 14:25:00 -0400

    Updated the Crew List.

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -9,6 +9,7 @@ export class CrewComponent implements OnInit {
 
   inCrew: boolean = false;
   crew: object[] = [];
+  mouseSelect: object = null;
 
   candidates: object[] = [
     {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
